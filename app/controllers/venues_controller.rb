bd3c08f0dd4dff766b9d34f949cf38345b0bc512
Jonas Forsberg <himynameisjonas@gmail.com>
2010-10-08 10:51:44 +0200

    order on venue#index

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -2,7 +2,7 @@ class VenuesController < ApplicationController
   # GET /venues
   # GET /venues.xml
   def index
-    @venues = Venue.all
+    @venues = Venue.order(:created_at).all
 
     respond_to do |format|
       format.html # index.html.erb
