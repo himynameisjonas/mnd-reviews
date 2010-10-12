0389db1a9841d41179ec5d4f902fa558251e15b6
Jonas Forsberg <himynameisjonas@gmail.com>
2010-10-12 13:22:22 +0200

    test igen

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
+  require 'memcached'
   require "openid/store/memcache"
   provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
   provider :GoogleApps, OpenID::Store::Memcache.new(Memcached::Rails.new), :domain => ENV['GOOGLE_DOMAIN']
